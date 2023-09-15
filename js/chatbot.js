afcdffe5af3b37eb12f070104adb2d2769bc1882
140hz <oscar_king62@hotmail.com>
2023-09-15 11:17:36 -0600

    chatbot add

---
+++ js/chatbot.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("chat-form");
+const input = document.getElementById("chat-input");
+const messages = document.getElementById("chat-messages");
+const iave = "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";
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const message = input.value;
+  input.value = "";
+
+  messages.innerHTML += `<div class="message user-message">
+  <img src="./icons/user.gif" alt="user icon"> <span>${message}</span>
+  </div>`;
+
+  // Use axios library to make a POST request to the OpenAI API
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: message,
+      model: "text-davinci-003",
+      temperature: 0,
+      max_tokens: 1000,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.0,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${iave}`,
+      },
+    }
+  );
+  const chatbotResponse = response.data.choices[0].text;
+
+  messages.innerHTML += `<div class="message bot-message">
+  <img src="./icons/bot.gif" alt="bot icon"> <span>${chatbotResponse}</span>
+  </div>`;
+});
